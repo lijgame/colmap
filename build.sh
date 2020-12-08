e50979683131e560fefd09415b5ea94d4532e92c
Li Jiang <lijgame@gmail.com>
2020-12-08 23:42:39 +0800

    update build script for jetson

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/codes/colmap/build
 cd ~/codes/colmap/build
-# cmake -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9 DCMAKE_CXX_COMPILER=/usr/bin/ld.lld -DTESTS_ENABLED=OFF ~/codes/colmap
-cmake -DTESTS_ENABLED=OFF ~/codes/colmap
+cmake -DCMAKE_C_COMPILER=/usr/bin/clang-8 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-8 DCMAKE_CXX_COMPILER=/usr/bin/ld.lld-8 -DTESTS_ENABLED=OFF ~/codes/colmap
+#cmake -DTESTS_ENABLED=OFF ~/codes/colmap
 make -j
